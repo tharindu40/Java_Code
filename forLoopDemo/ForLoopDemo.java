477f2a984e3a0dedfbbd70daf77efd904411ad98
shafi <mdshafiuddin935@gmail.com>
2023-10-02 12:26:50 +0530

    added for-Loop-ExampleProgram

---
+++ forLoopDemo/ForLoopDemo.java
@@ -0,0 +1,12 @@
+package forLoopDemo;
+
+public class ForLoopDemo {
+    public static void main(String[] args) {
+
+        int n = 5;
+        // for loop
+        for (int i = 1; i <= n; ++i) {
+            System.out.println("Java is fun");
+        }
+    }
+}
