ed1ae584b76aaef85faf4eb52253be288d56dfd8
akalanka <akalanka@echonlabs.com>
2023-10-14 20:32:31 +0530

    main.java create in test folder

---
+++ test/main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Your Java program logic goes here
+        System.out.println("Hello, World!"); // You can replace this with your code
+    }
+}
