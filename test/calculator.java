1770d5e9c08f3e9877b028684a0b0e4aab9bc0b3
akalanka <akalanka@echonlabs.com>
2023-10-14 20:39:13 +0530

    calculator.java class created

---
+++ test/calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public double divide(int num1, int num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return (double) num1 / num2;
+    }
+}
