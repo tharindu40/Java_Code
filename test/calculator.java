627c4d9a79fd64289e39bc9c73bbf626084d12b2
Tharindu37 <tharindut520@gmail.com>
2023-10-15 19:41:43 +0530

    create multiply method

--- test/calculator.java
+++ test/calculator.java
@@ -2,9 +2,13 @@ public class Calculator {
     public int add(int num1, int num2) {
         return num1 + num2;
     }
-    
+
     public int subtract(int num1, int num2) {
         return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
     }
 
 }
