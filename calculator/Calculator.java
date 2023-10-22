ea3202fa6690cc79260e7f8c3e5d8a116673541e
Shruti Singh <96919042+ShrutiSingh04503@users.noreply.github.com>
2023-10-22 13:53:31 +0530

    Update Calculator.java

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -10,6 +10,8 @@ class Calculator{
 
     public int multiplication(int num1,int num2){
         return num1*num2;
-    
 
 }
+    public int division (int num1, int num2){
+    return num1/num2;
+    }
