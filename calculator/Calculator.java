f7e534ccb569b7f18846f97a73c2b94abac91588
Navodi99 <navodil99@gmail.com>
2023-10-22 13:11:01 +0530

    Insert substraction method

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -3,4 +3,9 @@ class Calculator{
     public int addition(int num1,int num2){
         return num1+num2;
     }
+
+    public int substraction(int num1,int num2){
+        return num1-num2;
+    }
+
 }
